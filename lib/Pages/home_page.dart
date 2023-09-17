03122f52713bbe22ebc5161ccbf6f362107c5eb3
shashank4115 <shashankbhuma4115@gmail.com>
2023-09-17 11:45:30 +0530

    nav bar

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:ridesharev2/Pages/scan.dart';
 
 class HomePage extends StatefulWidget {
@@ -26,11 +27,48 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Column(
         children: [
-          ElevatedButton(onPressed: () async {
-            await availableCameras().then((value) => Navigator.push(context,
-                MaterialPageRoute(builder: (_) => Scan(cameras: value))));
-          }, child: Text("Camera"))
+          ElevatedButton(
+              onPressed: () async {
+                await availableCameras().then((value) => Navigator.push(context,
+                    MaterialPageRoute(builder: (_) => Scan(cameras: value))));
+              },
+              child: Text("Camera"))
         ],
+      ),
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
+          child: GNav(
+            backgroundColor: Colors.black,
+            color: Colors.white,
+            activeColor: Colors.white,
+            tabBackgroundColor: Colors.grey.shade700,
+            padding: EdgeInsets.all(16),
+            gap: 10,
+            onTabChange: (index) {
+              print(index);
+            },
+            tabs: const [
+              GButton(
+                icon: Icons.home,
+                text: 'home',
+              ),
+              GButton(
+                icon: Icons.camera_alt,
+                text: 'scan',
+              ),
+              GButton(
+                icon: Icons.comment,
+                text: 'comments',
+              ),
+              GButton(
+                icon: Icons.account_circle_outlined,
+                text: 'profile',
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
